7571c188f25229e48f4a47a494228717621d39a2
Frederick Ernesto Borges Noronha <frederickborgesnoronha@gmail.com>
2020-03-23 23:17:31 +0100

    Ejercicios 24 y 25

--- Ejercicios/main.cpp
+++ Ejercicios/main.cpp
@@ -34,8 +34,6 @@ int numPicos(const Lista<int>& lista){
 	return cont;
 }
 
-
-
 // EJERCICIO 23
 // función que duplica los elementos de una lista de enteros
 // si la lista es [1,2,3] la transforma en [1,1,2,2,3,3] 
@@ -48,7 +46,57 @@ void repiteElementos (Lista<int> &lista) {
 }
 
 
-int main() {
+// EJERCICIO 24
+// subprograma que reciba dos listas de enteros ordenados crecientemente 
+// y devuelva una nueva lista ordenada con la unión de los enteros de las 
+// dos listas iniciales
+Lista<int> mezclar(const Lista<int>& l1, const Lista<int>& l2){
+	Lista<int> lista;
+
+	Lista<int>::ConstIterator it1 = l1.cbegin();  
+	Lista<int>::ConstIterator it2 = l2.cbegin();  
+
+	while ((it1 != l1.cend()) && (it2 != l2.cend())) {
+		if (it1.elem() < it2.elem()){
+			lista.pon_final(it1.elem());
+			it1.next();
+		}
+		else{
+			lista.pon_final(it2.elem());
+			it2.next();
+		}
+	}
+
+	if (it1 == l1.cend()){
+		while (it2 != l2.cend()){
+			lista.pon_final(it2.elem());
+			it2.next();
+		}
+	}
+	else{
+		while (it1 != l1.cend()){
+			lista.pon_final(it1.elem());
+			it1.next();
+		}
+	}
+
+	return lista;
+}
+
+
+// EJERCICIO 25
+// subprograma que elimina los números pares de una lista de enteros
+void quitaPares(Lista<int>& lista) {
+	Lista<int>::Iterator it = lista.begin();  
+	while (it != lista.end()) {
+		if (it.elem() % 2 == 0)
+			it = lista.eliminar(it);
+		else
+			it.next();
+	}
+}
+
+void mainEj21y23(){
 	// listas para las pruebas
 	Lista<int> listaVacia, lista1, lista2;
 		
@@ -103,8 +151,82 @@ int main() {
 	repiteElementos(lista1);
 	pinta(lista1);
 	cout << endl;
+}
+
+void mainEj24y25(){
+	// listas para las pruebas
+	Lista<int> listaVacia, lista1, lista2, lo1, lo2;
+
+	for (int i = 1; i <= 6; i++) 
+		for (int repeticiones = 1; repeticiones <=2; repeticiones++)
+			lista1.pon_final(i);
+		
+	for (int i = 2; i <= 6; i = i + 2)
+		lista2.pon_ppio(i);
+
+	for (int i = 1; i < 10; i = i + 2)
+		lo1.pon_final(i);
+
+	for (int i = 0; i <= 10; i = i + 2)
+		lo2.pon_final(i);
+
+	cout << "\n\n--- EJ24: uso de MEZCLAR para MEZCLAR DOS LISTAS ORDENADAS ---\n";
+	
+	cout << "Cuando mezclo dos listas vacias resulta\n";
+	pinta(mezclar(listaVacia, listaVacia));
+	cout << endl;
+
+	cout << "Cuando mezclo la lista vacia con \n";
+	pinta(lo1);
+	cout << "Resulta\n";
+	pinta(mezclar(listaVacia, lo1));
+
+	cout << "Cuando mezclo\n";
+	pinta(lo1);
+	cout << "con la lista vacia resulta\n";
+	pinta(mezclar(lo1, listaVacia));
+
+	cout << "Cuando mezclo\n";
+	pinta(lo1);
+	cout << "con\n";
+	pinta(lo2);
+	cout << "Resulta\n";
+	pinta(mezclar(lo1, lo2));
+
+	cout << "Cuando mezclo\n";
+	pinta(lo2);
+	cout << "con\n";
+	pinta(lo1);
+	cout << "Resulta\n";
+	pinta(mezclar(lo2, lo1));
+
+	cout << "\n\n--- EJ25: uso de ELIMINAR para ELIMINAR LOS PARES DE lista ---\n";
+
+	cout << "Cuando elimino los numeros pares de la lista vacia resulta\n";
+	quitaPares(listaVacia);
+	pinta(listaVacia);
+	cout << endl;
+
+	cout << "Cuando elimino los numeros pares de la lista\n";
+	pinta(lista1);
+	cout << "Resulta\n";
+	quitaPares(lista1);
+	pinta(lista1);
+	cout << endl;
+
+	cout << "Cuando elimino los numeros pares de la lista\n";
+	pinta(lista2);
+	cout << "Resulta\n";
+	quitaPares(lista2);
+	pinta(lista2);
+	cout << endl;
+}
 
 
-	system("PAUSE");
+int main() {
+	
+	mainEj21y23();
+	mainEj24y25();
+	
 	return 0;
 }
